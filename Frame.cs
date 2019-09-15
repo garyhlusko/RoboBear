e1dec3f4e4a07e8add208cab62fbc782715ff5dd
garyhlusko <gary.hlusko@gmail.com>
2019-09-15 12:49:43 -0400

    fixing path finding, json, and printing issues

--- Frame.cs
+++ Frame.cs
@@ -1,85 +1,129 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft;
+using System.Linq;
+using Newtonsoft.Json;
 
-
-public class Frame
+class Frame
 	{
-		public int Index {get; set;};
-		public string[] Headers{get; set;};
-		public Json Data{get; set;};
+        private int Index;
+        private List<string> Headers;
+        private string Data;
 
-		public Frame(int Index, string[] headers, Json Data)
+		public Frame(int Index, List<string> Headers, string Data)
 		{
-			_Index = Index;
-			_Headers = Headers;
-			_Data = Data;
+			this.Index = Index;
+			this.Headers = Headers;
+			this.Data = Data;
 		}
 		
-		public static void Print(){
-
+		public void Print()
+        {
+            foreach(string header in Headers)
+            {
+                Console.Write(header + "\t");
+            }
 		}
 
 		//filter column
-		public static Frame FilterColumn(string ColumnName,string value)
+		public void FilterColumn( string ColumnName, string Value)
 		{
-			Frame return_frame;
-
+            Frame ReturnFrame;
 		}
 
 		//compare if it's different return true, if it's false reutrn false
-		public static Frame Compare(Frame CompareFrame)
-		{	
-
+		public Frame Compare(Frame CompareFrame)
+		{
+            Frame ReturnFrame = CompareFrame;
+            return ReturnFrame;
 		}
 
 		//diff return Frame of differences
-		public static Frame Diff(Frame DiffFrame)
+		public Frame Diff(Frame DiffFrame)
 		{
-
+            Frame ReturnFrame = DiffFrame;
+            return ReturnFrame;
 		}
 
 
 	}
 
-public static Json GetJson(string[] rows, string[] Headers)
-{
-	Dictionary<string,string> values = new Dictionary <string,string>();
-	foreach(string zipped in System.Collections.Generic.IEnumerable<string,string>(rows,Headers);)
-	{
-		values.add(zipped[0],zipped[1]);
-	}
-	Json json = JsonConvert.SerializeObject(values);
-	return json 
-}
 
 class Program{
 	static void Main()
 	{
-		
+        Console.WriteLine("Entering Path");
+        Frame df = read_csv("C:\\Users\\garyh\\Desktop\\test_csv.csv");
+        Console.WriteLine(df);
+        df.Print();
+        Console.ReadLine();
 	}
 	
-	static Frame read_csv(string path, delimiter=",",Headers=0,index=false)
+	static Frame read_csv(string Path, char Delimiter=',', Nullable<int> Headers=0, Nullable<int> Index=0)
 	{
-		Frame f;
-		using(var reader = new StreamReader(@"{path}"))
+		Frame F;
+        List<string> HeaderData = new List<string>();
+        Dictionary<string,string> Data = new Dictionary<string,string>();
+
+        string result = System.IO.Path.GetFullPath(Path);
+
+        Console.ReadLine();
+		using(var reader = new StreamReader(result))
 		{
 			int i = 0;
 			while(!reader.EndOfStream)
 			{
+                int LineNumber = 0;
 				var line = reader.ReadLine();
-				var values = line.Split(delimiter);
+				var values = line.Split(Delimiter);
 				
-				if(Headers is not None & i == 0){
-					for(string column in values){
-						f.Headers.add(column);
+				if(Headers != null & i.Equals(Headers)){
+					foreach(string column in values){
+						HeaderData.Add(column);
 					}
-					i+= 1
+                    i += 1;
+				}
+                if(Headers == null & i.Equals(0))
+                {
+                    int j = 0;
+                    foreach(string column in values)
+                    {
+                        HeaderData.Add(j.ToString());
+                    }
+                }
+
+				if(Index.HasValue){
+					Data[values[Index.Value]] = GetJson(values, HeaderData);
 				}
 
-				if(index){
-					f.Data[line[index]] = GetJson(string[] rows, f.Headers);
-				}			
+                if(Index == null)
+                {
+                    Data[LineNumber.ToString()] = GetJson(values, HeaderData);
+                    LineNumber += 1;
+                }
 			}	
 		}
-		return f;	
+
+        string DataJson = JsonConvert.SerializeObject(Data);
+        F = new Frame(Index.Value, HeaderData, DataJson);
+		return F;	
 	}
+
+
+    public static string GetJson(string[] Rows, List<string> Headers)
+    {
+
+        Dictionary<string, string> values = new Dictionary<string, string>();
+        var zipHeadersAndRow = Headers.Zip(Rows, (h, r) => new { Header = h, Row = r });
+        foreach (var zipped in zipHeadersAndRow)
+        {
+            string key = zipped.Header;
+            string value = zipped.Row;
+            values.Add(key, value);
+        }
+
+        string json = JsonConvert.SerializeObject(values);
+        return json;
+    }
 }
